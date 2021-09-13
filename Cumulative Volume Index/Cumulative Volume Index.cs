813847f586e4e11c74d1452f83af1a6945304070
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-09-13 11:24:02 +0300

    Changed calculation method

--- Cumulative Volume Index/Cumulative Volume Index.cs
+++ Cumulative Volume Index/Cumulative Volume Index.cs
@@ -6,37 +6,50 @@
 
 using cAlgo.API;
 using cAlgo.API.Internals;
+using System.Linq;
 
 namespace cAlgo
 {
     [Indicator(IsOverlay = false, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
     public class CumulativeVolumeIndex : Indicator
     {
-        private Bars _advancingBars, _decliningBars;
+        private Bars[] _stockBars;
 
-        [Parameter("Advancing Symbol", DefaultValue = "EURUSD")]
-        public string AdvancingSymbol { get; set; }
-
-        [Parameter("Declining Symbol", DefaultValue = "USDCAD")]
-        public string DecliningSymbol { get; set; }
+        [Parameter("Stock Symbols", DefaultValue = "AAPL,TSLA,AMZN,MSFT")]
+        public string StockSymbols { get; set; }
 
         [Output("Main")]
         public IndicatorDataSeries Result { get; set; }
 
         protected override void Initialize()
         {
-            _advancingBars = MarketData.GetBars(TimeFrame, AdvancingSymbol);
-            _decliningBars = MarketData.GetBars(TimeFrame, DecliningSymbol);
+            _stockBars = StockSymbols.Split(',').Select(symbol => MarketData.GetBars(TimeFrame, symbol)).ToArray();
         }
 
         public override void Calculate(int index)
         {
-            if (_advancingBars == null || _decliningBars == null) return;
+            if (_stockBars == null || _stockBars.Length == 0) return;
 
-            var advancingBarsIndex = _advancingBars.OpenTimes.GetIndexByTime(Bars.OpenTimes[index]);
-            var decliningBarsIndex = _decliningBars.OpenTimes.GetIndexByTime(Bars.OpenTimes[index]);
+            var advancingStocks = 0;
+            var decliningStocks = 0;
 
-            var diff = _advancingBars.ClosePrices[advancingBarsIndex] - _decliningBars.ClosePrices[decliningBarsIndex];
+            foreach (var stockBars in _stockBars)
+            {
+                var stockBarIndex = stockBars.OpenTimes.GetIndexByTime(Bars.OpenTimes[index]);
+
+                if (stockBarIndex == 0) continue;
+
+                if (stockBars.ClosePrices[stockBarIndex] > stockBars.ClosePrices[stockBarIndex - 1])
+                {
+                    advancingStocks++;
+                }
+                else
+                {
+                    decliningStocks++;
+                }
+            }
+
+            var diff = advancingStocks - decliningStocks;
 
             Result[index] = index == 0 ? diff : Result[index - 1] + diff;
         }
